88d6668a6775cc1b32ff0d4cf6771dd978302212
Abelino05 <pakpaliabel@gmail.com>
2022-08-10 00:58:38 +0000

    Adding other files

--- _printf.c
+++ _printf.c
@@ -1,48 +1,92 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _printf - Function that prints as the nartive printf
- * @format: Paramater to print
+ * printIdentifiers - prints special characters
+ * @next: character after the %
+ * @arg: argument for the indentifier
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+
+int printIdentifiers(char next, va_list arg)
+{
+	int functsIndex;
+
+	identifierStruct functs[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"d", print_int},
+		{"i", print_int},
+		{"u", print_unsigned},
+		{"b", print_unsignedToBinary},
+		{"o", print_oct},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"S", print_STR},
+		{NULL, NULL}
+	};
+
+	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
+	{
+		if (functs[functsIndex].indentifier[0] == next)
+			return (functs[functsIndex].printer(arg));
+	}
+	return (0);
+}
+
+/**
+ * _printf - mimic printf from stdio
+ * Description: produces output according to a format
+ * write output to stdout, the standard output stream
+ * @format: character string composed of zero or more directives
  *
- * return: The number of character printed
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ * return -1 for incomplete identifier error
  */
 
 int _printf(const char *format, ...)
 {
-	int i;
-	int count = 0;
-	va_list My_list;
+	unsigned int i;
+	int identifierPrinted = 0, charPrinted = 0;
+	va_list arg;
 
-	va_start(My_list, format);
-
-	/*if (format == NULL)
-		return (-1);*/
+	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		for (;format[i] == '%'; i++)
-			switch (format[i + 1])
-			{
-				case 'c':
-					count += print_char(My_list);
-					i += 1;
-					break;
-				case 's':
-					count += print_str(My_list);
-					i++;
-					break;
-				default:
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					i += 1;
-			}
-		if (format[i])
+		if (format[i] != '%')
 		{
 			_putchar(format[i]);
-			count++;
+			charPrinted++;
+			continue;
 		}
+		if (format[i + 1] == '%')
+		{
+			_putchar('%');
+			charPrinted++;
+			i++;
+			continue;
+		}
+		if (format[i + 1] == '\0')
+			return (-1);
 
+		identifierPrinted = printIdentifiers(format[i + 1], arg);
+		if (identifierPrinted == -1 || identifierPrinted != 0)
+			i++;
+		if (identifierPrinted > 0)
+			charPrinted += identifierPrinted;
+
+		if (identifierPrinted == 0)
+		{
+			_putchar('%');
+			charPrinted++;
+		}
 	}
-	va_end(My_list);
-	return (count);
+	va_end(arg);
+	return (charPrinted);
 }
--- print_char.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-int print_char(va_list list)
-{
-	_putchar(va_arg(list, int));
-	return (0);
-}
